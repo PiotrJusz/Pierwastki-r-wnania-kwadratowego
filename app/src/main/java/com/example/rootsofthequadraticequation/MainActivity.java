e19a9e8571c637a644521460acbf35157e15455f
PiotrJusz <94100033+PiotrJusz@users.noreply.github.com>
2022-02-16 06:04:16 +0000

    Porządkowanie kodu i usunięcie warnings

--- app/src/main/java/com/example/rootsofthequadraticequation/MainActivity.java
+++ app/src/main/java/com/example/rootsofthequadraticequation/MainActivity.java
@@ -2,19 +2,11 @@ package com.example.rootsofthequadraticequation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
-//import android.widget.RadioButton;
-import android.provider.MediaStore;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.view.View;
 import android.widget.TextView;
 
 import java.util.Locale;
@@ -32,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         double x1;
         double x2;
         String[] answer = new String[4];
-        String langInfo = String.valueOf(Locale.getDefault().getLanguage());
+        String langInfo = Locale.getDefault().getLanguage();
         if ("pl".equals(langInfo)){
             result[0] = "Równanie nie posiada rozwiązań.";
             result[1] = "Równanie posiada jedno rozwiązanie.";
@@ -44,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
             result[2]="The roots are real and distinct.";
         }
 
-        Float delta;
+        float delta;
         delta = b*b - 4*a*c;
         answer[0]=Float.toString(delta);
         if (a == 0) {
@@ -66,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
                 answer[1] = result[1];
                 x1 = -c / b;
                 if (x1 == -0.0){
-                    x1 = Float.valueOf(0);
+                    x1 = 0;
                 }
                 answer[2] = Double.toString(x1);
                 answer[3] = "";
@@ -83,7 +75,7 @@ public class MainActivity extends AppCompatActivity {
             else if (delta == 0) {//jedno rozwiązanie dla funkcji kwadratowej, delta=0
                 x1 = Math.round((-b) / (2 * a));
                 if (x1 == -0) {
-                    x1 = Float.valueOf(0);
+                    x1 = 0;
                 }
                 answer[1] = result[1];
                 answer[2] = Double.toString(x1);
@@ -113,10 +105,10 @@ public class MainActivity extends AppCompatActivity {
         textView_x2.setText("");
 
         //zmiana wartości domyślnych jeśli jeśli istnieje wartość dla x1 lub x2
-        if (data[2] != ""){
+        if (!data[2].equals("")){
             textView_x1.setText(new StringBuilder().append("x1 = ").append(data[2]));
         }
-        if (data[3] != "") {
+        if (!data[3].equals("")) {
             //t.setText(Html.fromHtml("7<sup>2</sup>"));
             textView_x2.setText(new StringBuilder().append("x2 = ").append(data[3]));
         }
@@ -151,7 +143,7 @@ public class MainActivity extends AppCompatActivity {
             fFactorA = Float.parseFloat((factorA));
         }
         catch (NumberFormatException e){
-            fFactorA = Float.valueOf(0);
+            fFactorA = (float) 0;
             editTextNumberDecimal_A.setTextColor(Color.RED);
         }
         try {
@@ -159,7 +151,7 @@ public class MainActivity extends AppCompatActivity {
             fFactorB = Float.parseFloat(factorB);
         }
         catch (NumberFormatException e){
-            fFactorB = Float.valueOf(0);
+            fFactorB = (float) 0;
             editTextNumberDecimal_B.setTextColor(Color.RED);
         }
         try{
@@ -167,7 +159,7 @@ public class MainActivity extends AppCompatActivity {
             fFactorC = Float.parseFloat((factorC));
         }
         catch (NumberFormatException e){
-            fFactorC = Float.valueOf(0);
+            fFactorC = (float) 0;
             editTextNumberDecimal_C.setTextColor(Color.RED);
         }
         //tworzenie wyrażenia tekstowego przedstawiające wyrażenie funkcji (zmienic ten komentarz!)
@@ -197,15 +189,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editTextNumberDecimal_A = (EditText)findViewById(R.id.editTextNumberDecimal_A);
+        editTextNumberDecimal_A = findViewById(R.id.editTextNumberDecimal_A);
         defaultTextColor = editTextNumberDecimal_A.getCurrentTextColor();
-        editTextNumberDecimal_B = (EditText)findViewById(R.id.editTextNumberDecimal_B);
-        editTextNumberDecimal_C = (EditText)findViewById(R.id.editTextNumberDecimal_C);
-        textView_function = (TextView) findViewById(R.id.textView_function);
-        textView_delta = (TextView) findViewById(R.id.textView_delta);
-        textView_info = (TextView) findViewById(R.id.textView_info);
-        textView_x1 = (TextView) findViewById(R.id.textView_x1);
-        textView_x2 = (TextView) findViewById(R.id.textView_x2);
+        editTextNumberDecimal_B = findViewById(R.id.editTextNumberDecimal_B);
+        editTextNumberDecimal_C = findViewById(R.id.editTextNumberDecimal_C);
+        textView_function = findViewById(R.id.textView_function);
+        textView_delta = findViewById(R.id.textView_delta);
+        textView_info = findViewById(R.id.textView_info);
+        textView_x1 = findViewById(R.id.textView_x1);
+        textView_x2 = findViewById(R.id.textView_x2);
         //dodanie interfejsu TextChangedListener dla pol a,b i c
         //
         editTextNumberDecimal_A.addTextChangedListener(new TextWatcher() {
